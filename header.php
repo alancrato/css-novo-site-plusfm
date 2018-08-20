b8c27afca4194005915cfd720b13fd4d94656e0f
Alan Rodrigues <alancrato@hotmail.com>
2018-08-20 17:17:08 -0300

    init header nav menu states

--- header.php
+++ header.php
@@ -19,12 +19,12 @@
 
         <div class="row hide-on-small-only">
             <div class="col s2">
-                <a id="logo-container" href="#" class="brand-logo">
+                <a id="logo-container" href="javascript:void(null)" class="brand-logo">
                     <img src="assets/img/logo.png"/>
                 </a>
             </div>
             <div class="col s2 margin-vivo">
-                <a class="vivo center">Ao vivo</a>
+                <a href="javascript:void(null)" data-target="slide-out" class="center sidenav-trigger">Ao vivo</a>
             </div>
             <div class="col s8 margin-menu">
                 <ul class="right hide-on-med-and-down">
@@ -45,22 +45,250 @@
                 </a>
             </div>
             <div class="col s3">
-                <a class="vivo-mobile">Ao vivo</a>
+                <a href="javascript:void(null)" data-target="slide-out" class="center sidenav-trigger ao-vivo">Ao vivo</a>
             </div>
             <div class="col s3">
-                <a href="#" data-target="nav-mobile" class="sidenav-trigger"><i class="material-icons">menu</i></a>
+                <a href="javascript:void(null)" data-target="nav-mobile" class="sidenav-trigger menu"><i class="material-icons">menu</i></a>
             </div>
             <ul id="nav-mobile" class="sidenav">
-                <li><a href="#">Home</a></li>
-                <li><a href="#">Quem Somos</a></li>
-                <li><a href="#">Drops da Plus</a></li>
-                <li><a href="#">Programação</a></li>
-                <li><a href="#">Promoções</a></li>
-                <li><a href="#">Contato</a></li>
+                <li>
+                    <div class="user-view-cap">
+                        <img src="assets/img/mascote-plus-nav.png"/>
+                        <div class="legal">
+                            Aqui é legal demais!
+                        </div>
+                    </div>
+                </li>
+                <li><a href="javascript:void(null)"><i class="material-icons">home</i> Home</a></li>
+                <li><a href="javascript:void(null)"><i class="material-icons">music_video</i> Quem Somos</a></li>
+                <li><a href="javascript:void(null)"><i class="material-icons">textsms</i> Drops da Plus</a></li>
+                <li><a href="javascript:void(null)"><i class="material-icons">queue_music</i> Programação</a></li>
+                <li><a href="javascript:void(null)"><i class="material-icons">card_giftcard</i> Promoções</a></li>
+                <li><a href="javascript:void(null)"><i class="material-icons">settings_cell</i> Contato</a></li>
+                <li><a href="http://www.cearanews7.com" target="_blank"><i class="material-icons">web</i> Ceará News 7</a></li>
             </ul>
         </div>
     </div>
 </nav>
+
+<ul id="slide-out" class="sidenav states">
+    <li>
+        <div class="user-view-cap">
+            <img src="assets/img/mascote-plus-nav.png"/>
+            <div class="legal">
+                Aqui é legal demais!
+            </div>
+        </div>
+    </li>
+    </br>
+    <div class="row">
+        <div class="col s3">
+            <img src="assets/img/states/rede.png"/>
+        </div>
+        <div class="col s9">
+            <div class="row">
+                <div class="col s8">
+                    <div class="state">
+                        Rede
+                    </div>
+                    <div class="frequency">
+                        Plus
+                    </div>
+                </div>
+                <div class="col s4">
+                    <div class="ouvir">
+                        Ouvir
+                    </div>
+                </div>
+            </div>
+            <div class="line-bottom"></div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col s3">
+            <img src="assets/img/states/aracati.png"/>
+        </div>
+        <div class="col s9">
+            <div class="row">
+                <div class="col s8">
+                    <div class="state">
+                        Aracati
+                    </div>
+                    <div class="frequency">
+                        98.1
+                    </div>
+                </div>
+                <div class="col s4">
+                    <div class="ouvir">
+                        Ouvir
+                    </div>
+                </div>
+            </div>
+            <div class="line-bottom"></div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col s3">
+            <img src="assets/img/states/cariri.png"/>
+        </div>
+        <div class="col s9">
+            <div class="row">
+                <div class="col s8">
+                    <div class="state">
+                        Cariri
+                    </div>
+                    <div class="frequency">
+                        100.5
+                    </div>
+                </div>
+                <div class="col s4">
+                    <div class="ouvir">
+                        Ouvir
+                    </div>
+                </div>
+            </div>
+            <div class="line-bottom"></div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col s3">
+            <img src="assets/img/states/crateus.png"/>
+        </div>
+        <div class="col s9">
+            <div class="row">
+                <div class="col s8">
+                    <div class="state">
+                        Crateús
+                    </div>
+                    <div class="frequency">
+                        93.3
+                    </div>
+                </div>
+                <div class="col s4">
+                    <div class="ouvir">
+                        Ouvir
+                    </div>
+                </div>
+            </div>
+            <div class="line-bottom"></div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col s3">
+            <img src="assets/img/states/iguatu.png"/>
+        </div>
+        <div class="col s9">
+            <div class="row">
+                <div class="col s8">
+                    <div class="state">
+                        Iguatu/Cariús
+                    </div>
+                    <div class="frequency">
+                        91.5
+                    </div>
+                </div>
+                <div class="col s4">
+                    <div class="ouvir">
+                        Ouvir
+                    </div>
+                </div>
+            </div>
+            <div class="line-bottom"></div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col s3">
+            <img src="assets/img/states/paraipaba.png"/>
+        </div>
+        <div class="col s9">
+            <div class="row">
+                <div class="col s8">
+                    <div class="state">
+                        Paraipaba
+                    </div>
+                    <div class="frequency">
+                        88.7
+                    </div>
+                </div>
+                <div class="col s4">
+                    <div class="ouvir">
+                        Ouvir
+                    </div>
+                </div>
+            </div>
+            <div class="line-bottom"></div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col s3">
+            <img src="assets/img/states/redencao.png"/>
+        </div>
+        <div class="col s9">
+            <div class="row">
+                <div class="col s8">
+                    <div class="state">
+                        Redenção
+                    </div>
+                    <div class="frequency">
+                        98.7
+                    </div>
+                </div>
+                <div class="col s4">
+                    <div class="ouvir">
+                        Ouvir
+                    </div>
+                </div>
+            </div>
+            <div class="line-bottom"></div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col s3">
+            <img src="assets/img/states/santa_quiteria.png"/>
+        </div>
+        <div class="col s9">
+            <div class="row">
+                <div class="col s8">
+                    <div class="state">
+                        Santa Quitéria
+                    </div>
+                    <div class="frequency">
+                        106.5
+                    </div>
+                </div>
+                <div class="col s4">
+                    <div class="ouvir">
+                        Ouvir
+                    </div>
+                </div>
+            </div>
+            <div class="line-bottom"></div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col s3">
+            <img src="assets/img/states/sobral.png"/>
+        </div>
+        <div class="col s9">
+            <div class="row">
+                <div class="col s8">
+                    <div class="state">
+                        Sobral
+                    </div>
+                    <div class="frequency">
+                        105.1
+                    </div>
+                </div>
+                <div class="col s4">
+                    <div class="ouvir">
+                        Ouvir
+                    </div>
+                </div>
+            </div>
+            <div class="line-bottom"></div>
+        </div>
+    </div>
+</ul>
 
 <?php
     require 'slide.php';
