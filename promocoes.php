3ed7101cc4cc35269e4f695cfc9aa4777665c256
Alan Rodrigues <alancrato@hotmail.com>
2018-08-21 17:14:45 -0300

    finalizando promocoes

---
+++ promocoes.php
@@ -0,0 +1,110 @@
+<?php
+    require 'header-about.php';
+?>
+
+<div class="header-category"></div>
+
+<div class="container">
+    <div class="row">
+        <div class="pub-desktop hide-on-small-only">
+            PUBLICIDADE
+        </div>
+        <div class="pub-mobile hide-on-med-and-up">
+            PUBLICIDADE
+        </div>
+    </div>
+</div>
+
+<div class="container">
+    <div class="row">
+        <div class="col s12 m3"></div>
+        <div class="col s12 m6">
+            <div class="drops-da-plus">
+                Promoções
+            </div>
+            <div class="subtitle-prog-da-plus">
+                Confira abaixo todas as nossas Promoções.
+            </div>
+        </div>
+        <div class="col s12 m3"></div>
+    </div>
+
+
+    <div class="row">
+        <div class="col s12 m8">
+            <div class="col s12 m6">
+                <div class="card">
+                    <div class="card-image">
+                        <img src="assets/img/card_promocao_01@3x.png">
+                        <span class="card-title">Promoção Encerrada</span>
+                    </div>
+                    <div class="card-date">
+                        17/08/2018 13:45
+                    </div>
+                    <div class="card-title">
+                        <a href="javascript:void(null)">
+                            I am a very simple card
+                        </a>
+                    </div>
+                    <div class="card-content">
+                        <a href="javascript:void()">
+                            I am a very simple card. I am good at containing small bits of information
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div class="col s12 m6">
+                <div class="card">
+                    <div class="card-image">
+                        <img src="assets/img/card_promocao_02@3x.png">
+                        <span class="card-title">Promoção Encerrada</span>
+                    </div>
+                    <div class="card-date">
+                        17/08/2018 13:45
+                    </div>
+                    <div class="card-title">
+                        <a href="javascript:void(null)">
+                            I am a very simple card
+                        </a>
+                    </div>
+                    <div class="card-content">
+                        <a href="javascript:void()">
+                            I am a very simple card. I am good at containing small bits of information
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div class="col s12 m6">
+                <div class="card">
+                    <div class="card-image">
+                        <img src="assets/img/card_promocao_01@3x.png">
+                        <span class="card-title">Promoção Encerrada</span>
+                    </div>
+                    <div class="card-date">
+                        17/08/2018 13:45
+                    </div>
+                    <div class="card-title">
+                        <a href="javascript:void(null)">
+                            I am a very simple card
+                        </a>
+                    </div>
+                    <div class="card-content">
+                        <a href="javascript:void()">
+                            I am a very simple card. I am good at containing small bits of information
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="col s12 m4">
+            <div class="pub-desktop2">
+                PUBLICIDADE
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php
+     require 'footer.php';
+?>
