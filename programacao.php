ed52714e22aa2ab0b19d42dcead6b1c210483a74
Alan Rodrigues <alancrato@hotmail.com>
2018-08-21 16:46:45 -0300

    ajustes css programacao

--- programacao.php
+++ programacao.php
@@ -1,7 +1,113 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: alanc
- * Date: 21/08/2018
- * Time: 15:19
- */
+require 'header-about.php';
+?>
+
+<div class="header-category"></div>
+
+<div class="container">
+    <div class="row">
+        <div class="col s12 m3"></div>
+        <div class="col s12 m6">
+            <div class="release-da-plus">
+                Programação
+            </div>
+            <div class="subtitle-prog-da-plus">
+                A melhor programação para você
+            </div>
+        </div>
+        <div class="col s12 m3"></div>
+    </div>
+</div>
+
+<div class="container programacao hide-on-small-only">
+    <div class="row">
+        <div class="col m3">
+            <img src="assets/img/programas/programa_01@2x.png"/>
+        </div>
+        <div class="col m3">
+            <img src="assets/img/programas/programa 02@2x.png"/>
+        </div>
+        <div class="col m3">
+            <img src="assets/img/programas/programa%2003@2x.png"/>
+        </div>
+        <div class="col m3">
+            <img src="assets/img/programas/programa%2004@2x.png"/>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col m3">
+            <img src="assets/img/programas/programa%2005@2x.png"/>
+        </div>
+        <div class="col m3">
+            <img src="assets/img/programas/programa%2006@2x.png"/>
+        </div>
+        <div class="col m3">
+            <img src="assets/img/programas/programa%2007@2x.png"/>
+        </div>
+        <div class="col m3">
+            <img src="assets/img/programas/programa%2008@2x.png"/>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col m3">
+            <img src="assets/img/programas/programa%2009@2x.png"/>
+        </div>
+        <div class="col m3">
+            <img src="assets/img/programas/programa%2010@2x.png"/>
+        </div>
+        <div class="col m3">
+            <img src="assets/img/programas/programa-1@2x-noite.png"/>
+        </div>
+        <div class="col m3">
+            <img src="assets/img/programas/programa%2011@2x.png"/>
+        </div>
+    </div>
+</div>
+
+<div class="container programacao-mobile hide-on-med-and-up">
+
+    <div class="carousel carousel-slider center">
+        <div class="carousel-item transparent" href="#one!">
+            <img src="assets/img/programas/programa_01@2x.png"/>
+        </div>
+        <div class="carousel-item transparent" href="#two!">
+            <img src="assets/img/programas/programa 02@2x.png"/>
+        </div>
+        <div class="carousel-item transparent" href="#three!">
+            <img src="assets/img/programas/programa%2003@2x.png"/>
+        </div>
+        <div class="carousel-item transparent" href="#four!">
+            <img src="assets/img/programas/programa%2004@2x.png"/>
+        </div>
+        <div class="carousel-item transparent" href="#five!">
+            <img src="assets/img/programas/programa%2005@2x.png"/>
+        </div>
+        <div class="carousel-item transparent" href="#six!">
+            <img src="assets/img/programas/programa%2006@2x.png"/>
+        </div>
+        <div class="carousel-item transparent" href="#seven!">
+            <img src="assets/img/programas/programa%2007@2x.png"/>
+        </div>
+        <div class="carousel-item transparent" href="#eight!">
+            <img src="assets/img/programas/programa%2008@2x.png"/>
+        </div>
+        <div class="carousel-item transparent" href="#nine!">
+            <img src="assets/img/programas/programa%2009@2x.png"/>
+        </div>
+        <div class="carousel-item transparent" href="#ten!">
+            <img src="assets/img/programas/programa%2010@2x.png"/>
+        </div>
+        <div class="carousel-item transparent" href="#eleven!">
+            <img src="assets/img/programas/programa-1@2x-noite.png"/>
+        </div>
+        <div class="carousel-item transparent" href="#twelve!">
+            <img src="assets/img/programas/programa%2011@2x.png"/>
+        </div>
+    </div>
+
+</div>
+
+
+<?php
+require 'footer.php';
+?>
